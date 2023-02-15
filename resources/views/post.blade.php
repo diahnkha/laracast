9c0a39c83b2aba432ca8b6c84f44a02c24bf6af8
Diah Nur Khasanah <diahnurkhasanah@Diahs-MacBook-Pro.local>
2023-02-15 16:59:00 +0700

    add initial project

---
+++ resources/views/post.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/app.css">
+    <title>my blog</title>
+</head>
+<body>
+    <article>
+
+        <h1><?= $post->title; ?></h1>
+
+        <div>
+            <?= $post->body; ?>
+        </div>
+
+        <a href="/">Go back</a>
+    </article>
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,158 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Models\Post;
+use Spatie\YamlFrontMatter\YamlFrontMatter;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Route::get('/', function(){
+//     $posts = Post::all();
+
+//     // ddd($posts[0]->getPathname())
+//     // dd($posts);
+//     // dd($posts[0]->getContents());
+
+//     return view('posts', [
+//         'posts' => $posts
+//     ]);
+// });
+
+// Route::get('/', function(){
+//     $document = YamlFrontMatter::parsefile(
+//         resource_path('posts/my-fourth-post.html')
+//     );
+
+//     dd($document);
+//     // dd($document->body());
+//     // dd($document->Matter('title'));
+//     //  dd($document->date);
+
+// });
+
+// Route::get('/', function(){
+//         //ngambil direktori langsung ke resource , gunanya file dan files itu buat scan semua yang ada di directory posts
+//     $files = File::files(resource_path("posts"));
+//     $documents = [];
+
+//     foreach ($files as $file){
+//         $documents[] = YamlFrontMatter::parsefile($file);
+
+//         // $posts[] = new Post(
+//         //     $document->title,
+//         //     $document->excerpt,
+//         //     $document->date,
+//         //     $document->body()
+//         // );
+//     };
+
+//     dd($documents);
+
+// //     return view('posts', [
+// //         'posts' => $posts
+// //     ]);
+// });
+
+// Route::get('/', function(){
+//     //ngambil direktori langsung ke resource , gunanya file dan files itu buat scan semua yang ada di directory posts
+//     $files = File::files(resource_path("posts"));
+//     $posts = [];
+
+//     $posts = array_map(function($file){
+//         $document = YamlFrontMatter::parsefile($file);
+
+//         return new post(
+//             $document->title,
+//             $document->excerpt,
+//             $document->date,
+//             $document->body(),
+//             $document->slug
+//         );
+//     },$files);
+
+//     // foreach ($files as $file){
+//     //     $document = YamlFrontMatter::parsefile($file);
+
+//     //     $posts[] = new Post(
+//     //         $document->title,
+//     //         $document->excerpt,
+//     //         $document->date,
+//     //         $document->body(),
+//     //         $document->slug
+//     //     );
+//     // };
+
+//     // dd($posts);
+//     // dd($posts[0]->title);
+
+//     return view('posts', [
+//         'posts' => $posts
+//     ]);
+// });
+
+Route::get('/', function(){
+    return view('posts',[
+        'posts' => Post::all()
+    ]);
+});
+
+Route::get('/posts/{post}', function ($slug) {
+
+    //find a post by its slug and pass it to a biew called "post"
+
+    // $post = Post::find($slug);
+
+    return view('post', [
+        'post' => Post::find($slug)
+    ]);
+
+    // $path = __DIR__ . "/../resources/posts/{$slug}.html";
+
+    // dd($path);
+
+
+    // if(! file_exists($path)){
+       
+    //     return redirect('/');
+    // }
+
+    // $post = cache()->remember("posts.{$slug}", 1200, function () use ($path){
+    //     var_dump('file_get_contents');
+    //     return file_get_contents($path);
+    // });
+
+    // $post = cache()->remember("posts.{$slug}", 1200, fn() => file_get_contents($path));
+    
+    // // $post = file_get_contents($path);
+
+    // return view('post', [
+    //     'isikonten' => $post
+    // ]);
+})->where('post', '[A-z_\-]+');
+// })->whereAlpha('post');
+
+// Route::get('/posts', function () {
+//     return [
+//         'foo' => 'bar'
+//     ];
+    
+// });
+
+Route::get('/coba/{haha}', function ($slug) {
+    return $slug;
+});
+
+
