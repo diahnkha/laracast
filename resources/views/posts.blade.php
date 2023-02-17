557c8b4f3bee071651985433c08bcb94e57f7280
Diah Nur Khasanah <diahnurkhasanah@Diahs-MacBook-Pro.local>
2023-02-17 11:41:09 +0700

    revert again

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -2,12 +2,13 @@
         @include ('_posts-header')
 
         <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
-            @if($posts->count())
-                <x-post-grid :posts="$posts"/>
+            @if ($posts->count())
+                    <x-post-grid :posts="$posts"/>
             @else
-                <p class="text-center">No posts yet. Please check back later.</p>
+                <p class="text-center">No Posts yet. Please check back later.</p>
             @endif
 
+<<<<<<< HEAD
             <div class="lg:grid lg:grid-cols-2">
                 <x-post-card />
                 <x-post-card />
@@ -15,10 +16,13 @@
 
 <<<<<<< HEAD
             <div class="lg:grid lg:grid-cols-6">
+=======
+            <!-- <div class="lg:grid lg:grid-cols-3">
+>>>>>>> parent of f859ed3... fix blade component and css grids
                 <x-post-card />
                 <x-post-card />
                 <x-post-card />
-            </div>
+            </div> -->
         </main>
 =======
             <p>
